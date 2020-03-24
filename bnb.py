d552f139122c6c12c258190b7afd7feca1df01d5
mdaffad <muhammaddaffadinaya@gmail.com>
2020-03-24 19:31:18 +0700

    method less

--- bnb.py
+++ bnb.py
@@ -4,18 +4,15 @@ LEFT = "LEFT"
 RIGHT = "RIGHT"
 def  swap(a,b):
 	a,b = b,a
-class matrix:
+class Matrix:
 	def __init__(self):
 		self.container = [[],[],[],[]]
 		self.cost = 0
 		self.lastMovement = ""
-	def __init__(self,other, inherit = True):
+	def __init__(self,other):
 		self.container = other.container
 		self.cost = other.cost
-		if (inherit):
-			self.lastMovement = other.lastMovement + 1
-		else:
-			self.lastMovement = other.lastMovement
+		self.lastMovement = other.lastMovement
 	def findBlank(self):
 		for i in range(4):
 			for j in range(4):
@@ -36,16 +33,35 @@ class matrix:
 			swap(self.container[blankX][blankY],self.container[blankX][blankY + 1])
 			self.lastMovement = RIGHT
 
-class matrixQueue:
+
+		# count cost LESS(X)
+		#self cost to movement
+		self.cost = self.cost + 1 + self.less()
+
+	def less(self):
+		result = 0
+		for i in range(4):
+			for j in range(4):
+				if self.container[i][j] != 0:
+					for k in range(i,4):
+						for l in range(j,4):
+							if self.container[i][j] > self.container[k][l] and self.container[k][l] != 0:
+								result = result + 1
+		return result
+	def inherit(self):
+		
+class MatrixQueue:
 	# As static atribbute
 	nodeExpand = 0
 	def __init__(self):
 		self.element = []
 	def add(self, newElement):
 		self.container.append(newElement)
-		nodeExpand = nodeExpand+1
+		nodeExpand = nodeExpand + 1
 f = open("test.txt", "r")
 inputMatrix = [[],[],[],[]]
+
+
 for i in range(4):
 	temp = f.readline().strip().split()
 	for j in range(4):
@@ -53,8 +69,7 @@ for i in range(4):
 			inputMatrix[i].append(int(temp[j]))
 		else:
 			inputMatrix[i].append(0)
-queue = []
-queue.append(inputMatrix)
-inputMatrix[1][0] = 4
-queue.append(inputMatrix)
-print(queue)
+baseNode = Matrix()
+baseNode.container = inputMatrix
+matrixQueue = MatrixQueue()
+ 
